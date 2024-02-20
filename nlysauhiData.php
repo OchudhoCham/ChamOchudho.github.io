52a7b55936e37c2480dfdcd7c2e489318577e4ec
OchudhoCham <ochudho.c@gmail.com>
2024-02-20 20:27:19 +0200

    First Commit

---
+++ nlysauhiData.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>NLYSAUHI DATA</title>
+</head>
+<body>
+<div class="form-container">
+  <div class="form-title">NLYSAUHI DATA</div>
+  <form>
+    <input type="text" class="form-input" name="full_name" placeholder="Full Name">
+    <input type="text" class="form-input" name="university" placeholder="University/Institute">
+    <input type="text" class="form-input" name="school" placeholder="School/College/Course">
+    <input type="text" class="form-input" name="department" placeholder="Department">
+    <input type="text" class="form-input" name="entry_year" placeholder="Entry year">
+    <input type="text" class="form-input" name="exit_year" placeholder="Exit year">
+    <input type="email" class="form-input" name="email" placeholder="Email">
+    <input type="tel" class="form-input" name="phone" placeholder="Phone">
+    <button type="submit" class="form-button">REGISTER</button>
+  </form>
+  <div class="form-footer"><a href="#"> Already a member?</div></a>
+</div>
+</body>
+</html>
